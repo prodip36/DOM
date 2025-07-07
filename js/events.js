7f98f6e24d8963135b58503dffe3292b34a3491a
nothing36 <prodipsarker953@gmail.com>
2025-07-07 12:52:28 +0600

    second commit

--- js/events.js
+++ js/events.js
@@ -1,1 +1,12 @@
 console.log('This is separate js file');
+// make red btn func
+function make_red(){
+    document.body.style.backgroundColor='red';
+}
+// make blue btn 
+const makeBlueButton= document.getElementById('make-blue');
+makeBlueButton.onclick= make_blue;
+function make_blue(){
+    document.body.style.backgroundColor='blue';
+}
+
